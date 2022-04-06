617fcd9104538df2ac357a244ea91a446123c21f
Fallanagel <havemedance@hotmail.com>
2022-04-05 20:51:02 -0500

    first

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement("h1")
+header.setAttibute("class", "dog-name")
+header.append("Rizzo") 
+content.append(header)
